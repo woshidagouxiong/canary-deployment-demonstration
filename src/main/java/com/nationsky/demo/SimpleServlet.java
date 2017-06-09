1affb39cbe183486fd7b834add87d9a6ace8a3db
zhangfulai <zhangfulai@nationsky.com>
2017-06-09 16:14:54 +0800

    no message

--- src/main/java/com/nationsky/demo/SimpleServlet.java
+++ src/main/java/com/nationsky/demo/SimpleServlet.java
@@ -7,14 +7,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class SimpleServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("<h1><font color=green>Simple Servlet ran successfully</font></h1>"
-                + "Powered by WebSphere Application Server Liberty Profile");
+        resp.getWriter().print("<h1><font color='green'>Welcome to Stable Version</font></h1>"
+                + "<p><span>Client&nbsp;IP:</span><span>" + getClientIp(req) + "</span></p>");
     }
 
+    private String getClientIp(HttpServletRequest req) {
+        String ip = req.getHeader("X-Forwarded-For");
+        if (StringUtils.isNotEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+            int index = ip.indexOf(",");
+            if (index != -1) {
+                return ip.substring(0, index);
+            } else {
+                return ip;
+            }
+        }
+
+        ip = req.getHeader("X-Real-IP");
+        if (StringUtils.isNotEmpty(ip) && !"unKnown".equalsIgnoreCase(ip)) {
+            return ip;
+        }
+
+        return req.getRemoteAddr();
+    }
 }
