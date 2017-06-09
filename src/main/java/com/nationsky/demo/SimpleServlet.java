fca7647ae1b13a2fa5f532f8403ec6fc6bcfb7d7
zhangfulai <zhangfulai@nationsky.com>
2017-06-09 15:53:40 +0800

    no message

---
+++ src/main/java/com/nationsky/demo/SimpleServlet.java
@@ -0,0 +1,20 @@
+package com.nationsky.demo;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("<h1><font color=green>Simple Servlet ran successfully</font></h1>"
+                + "Powered by WebSphere Application Server Liberty Profile");
+    }
+
+}
